3e7b4323b31fe1c78f9b843f5fa4960cf5cc87aa
Chad Schmutzer (schmutze@) <schmutze@amazon.com>
2016-11-29 11:24:15 -0800

    predict python updates

--- lab-2-build/mxnet/predict_imagenet.py
+++ lab-2-build/mxnet/predict_imagenet.py
@@ -1,28 +1,20 @@
-#!/usr/bin/env python 
+#!/usr/bin/env ipython 
 
-# coding: utf-8
-
-# # Predict with pre-trained models
-# 
-# This is a demo for predicting with a pre-trained model on the full imagenet dataset, which contains over 10 million images and 10 thousands classes. For a more detailed explanation, please refer to [predict.ipynb](https://github.com/dmlc/mxnet-notebooks/blob/master/python/how_to/predict.ipynb).
-# 
-# We first load the pre-trained model.
-
-# In[1]:
-
+from __future__ import print_function
 import os, sys, urllib
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
-print(sys.argv[1])
-sys.exit(0)
+if len(sys.argv) < 2:
+  print("Usage:", sys.argv[0], "<url>")
+  exit(0)
 
-#import mxnet as mx
+url = sys.argv[1]
+
+import mxnet as mx
 
 def download(url,prefix=''):
-    filename = prefix+url.split("/")[-1]
-    if not os.path.exists(filename):
-        urllib.urlretrieve(url, filename)
+  filename = prefix+url.split("/")[-1]
+  if not os.path.exists(filename):
+    urllib.urlretrieve(url, filename)
 
 path='http://data.mxnet.io/models/imagenet-11k/'
 download(path+'resnet-152/resnet-152-symbol.json', 'full-')
@@ -30,90 +22,45 @@ download(path+'resnet-152/resnet-152-0000.params', 'full-')
 download(path+'synset.txt', 'full-')
 
 with open('full-synset.txt', 'r') as f:
-    synsets = [l.rstrip() for l in f]
+  synsets = [l.rstrip() for l in f]
 
 sym, arg_params, aux_params = mx.model.load_checkpoint('full-resnet-152', 0)
-
-
-# Create a model for this model on GPU 0.
-
-# In[2]:
 
 mod = mx.mod.Module(symbol=sym, context=mx.cpu())
 mod.bind(for_training=False, data_shapes=[('data', (1,3,224,224))])
 mod.set_params(arg_params, aux_params)
 
-
-# Next we define the function to obtain an image by a given URL and the function for predicting.
-
-# In[3]:
-
-get_ipython().magic(u'matplotlib inline')
+get_ipython().magic(u'matplotlib auto')
 import matplotlib
 matplotlib.rc("savefig", dpi=100)
-import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 from collections import namedtuple
 Batch = namedtuple('Batch', ['data'])
 
 def get_image(url, show=True):
-    filename = url.split("/")[-1]
-    urllib.urlretrieve(url, filename)
-    img = cv2.imread(filename)
-    if img is None:
-        print('failed to download ' + url)
-    if show:
-        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        plt.axis('off')
-    return filename
+  filename = url.split("/")[-1]
+  urllib.urlretrieve(url, filename)
+  img = cv2.imread(filename)
+  if img is None:
+    print('failed to download ' + url)
+  return filename
 
 def predict(filename, mod, synsets):
-    img = cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB)
-    if img is None:
-        return None
-    img = cv2.resize(img, (224, 224))
-    img = np.swapaxes(img, 0, 2)
-    img = np.swapaxes(img, 1, 2) 
-    img = img[np.newaxis, :] 
+  img = cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB)
+  if img is None:
+    return None
+  img = cv2.resize(img, (224, 224))
+  img = np.swapaxes(img, 0, 2)
+  img = np.swapaxes(img, 1, 2) 
+  img = img[np.newaxis, :] 
     
-    mod.forward(Batch([mx.nd.array(img)]))
-    prob = mod.get_outputs()[0].asnumpy()
-    prob = np.squeeze(prob)
+  mod.forward(Batch([mx.nd.array(img)]))
+  prob = mod.get_outputs()[0].asnumpy()
+  prob = np.squeeze(prob)
 
-    a = np.argsort(prob)[::-1]    
-    for i in a[0:5]:
-        print('probability=%f, class=%s' %(prob[i], synsets[i]))
-    
+  a = np.argsort(prob)[::-1]    
+  for i in a[0:5]:
+    print('probability=%f, class=%s' %(prob[i], synsets[i]))
 
-
-# We are able to classify an image and output the top predicted classes. 
-
-# In[4]:
-
-url = 'http://writm.com/wp-content/uploads/2016/08/Cat-hd-wallpapers.jpg'
 predict(get_image(url), mod, synsets)
-
-
-# In[5]:
-
-url = 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg'
-predict(get_image(url), mod, synsets)
-
-
-# In[6]:
-
-url = 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg'
-predict(get_image(url), mod, synsets)
-
-
-# In[7]:
-
-url = 'https://lh3.googleusercontent.com/-k_xmPtk0Y2w/AAAAAAAAAAI/AAAAAAAACeA/tsBfyDtuVWU/photo.jpg'
-predict(get_image(url), mod, synsets)
-
-
-# In[ ]:
-
-
-
